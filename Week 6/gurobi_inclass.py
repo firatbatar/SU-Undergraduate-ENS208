cf49228ea2429a1117064f61f4e8573ca0862474
Fırat Batar <firat.o.batar@gmail.com>
2023-11-06 15:33:06 +0300

    Add week 6 inclass gurobi example

---
+++ Week 6/gurobi_inclass.py
@@ -0,0 +1,21 @@
+from gurobipy import GRB, Model
+
+m = Model('production')  # Create a production model
+
+# Add two continues varialbes (>= 0) into the model
+xc = m.addVar(vtype=GRB.CONTINUOUS, name="numOfChairs")  
+xt = m.addVar(vtype=GRB.CONTINUOUS, name="numOfTables")
+
+# Add constraints
+m.addConstr(xc + 2 * xt <= 50, "rawMaterial")
+m.addConstr(2* xc + xt <= 40, "workTime")
+
+# Set the objective
+m.setObjective(10 * xc + 10 * xt, GRB.MAXIMIZE)
+
+m.optimize()
+
+for v in m.getVars():
+    print(v.varName, v.x)
+
+print(f"The optimal objective function value is {m.objVal}")
